23361115462b0ad81465a570a2532c31f0924435
spear97 <72108425+spear97@users.noreply.github.com>
2024-02-21 15:21:49 -0600

    Update script.js

--- v1/guestbook/public/script.js
+++ v1/guestbook/public/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  // Selecting necessary elements from the DOM
   var headerTitleElement = $("#header h1");
   var entriesElement = $("#guestbook-entries");
   var formElement = $("#guestbook-form");
@@ -6,33 +7,40 @@ $(document).ready(function() {
   var entryContentElement = $("#guestbook-entry-content");
   var hostAddressElement = $("#guestbook-host-address");
 
+  // Function to append guestbook entries to the DOM
   var appendGuestbookEntries = function(data) {
-    entriesElement.empty();
+    entriesElement.empty(); // Clearing existing entries
     $.each(data, function(key, val) {
-      entriesElement.append("<p>" + val + "</p>");
+      entriesElement.append("<p>" + val + "</p>"); // Appending new entries
     });
   }
 
+  // Function to handle form submission
   var handleSubmission = function(e) {
-    e.preventDefault();
-    var entryValue = entryContentElement.val()
+    e.preventDefault(); // Preventing default form submission
+    var entryValue = entryContentElement.val(); // Getting the value from the input field
     if (entryValue.length > 0) {
-      entriesElement.append("<p>...</p>");
-      $.getJSON("rpush/guestbook/" + entryValue, appendGuestbookEntries);
-	  entryContentElement.val("")
+      entriesElement.append("<p>...</p>"); // Adding a temporary entry indicator
+      $.getJSON("rpush/guestbook/" + entryValue, appendGuestbookEntries); // Sending data to the server and updating guestbook
+      entryContentElement.val(""); // Clearing the input field after submission
     }
     return false;
   }
 
+  // Click event handler for the submit button
   submitElement.click(handleSubmission);
+
+  // Submit event handler for the form (in case Enter is pressed)
   formElement.submit(handleSubmission);
+
+  // Displaying the host address in the designated element
   hostAddressElement.append(document.URL);
 
-  // Poll every second.
+  // Polling the server every second to update guestbook entries
   (function fetchGuestbook() {
     $.getJSON("lrange/guestbook").done(appendGuestbookEntries).always(
       function() {
-        setTimeout(fetchGuestbook, 1000);
+        setTimeout(fetchGuestbook, 1000); // Polling interval of 1 second
       });
   })();
 });
